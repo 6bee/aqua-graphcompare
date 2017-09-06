a03d86ce8c0ddb93d8833c5b85c1da7f0ee9e8d6
Christof Senn <christof.senn@gmail.com>
2017-09-06 06:54:04 +0200

    put version-suffix in quotes if empty

--- pack.bat
+++ pack.bat
@@ -6,4 +6,4 @@ clean ^
   && dotnet build src\Aqua.GraphCompare --configuration %configuration% ^
   && dotnet build test\Aqua.GraphCompare.Tests --configuration %configuration% ^
   && dotnet test test\Aqua.GraphCompare.Tests\Aqua.GraphCompare.Tests.csproj --configuration %configuration% ^
-  && dotnet pack src\Aqua.GraphCompare\Aqua.GraphCompare.csproj --output "..\..\artifacts" --configuration %configuration% --include-symbols --version-suffix %version-suffix%
+  && dotnet pack src\Aqua.GraphCompare\Aqua.GraphCompare.csproj --output "..\..\artifacts" --configuration %configuration% --include-symbols --version-suffix "%version-suffix%"
