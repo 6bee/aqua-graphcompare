977dc86b332067b7de65791e27ce80578e773ea2
Christof Senn <christof.senn@gmail.com>
2023-08-01 02:24:00 +0200

    update build script

--- pack.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-set configuration=Debug
-clean ^
-  && dotnet test test\Aqua.GraphCompare.Tests --configuration %configuration% ^
-  && dotnet pack src\Aqua.GraphCompare        --configuration %configuration%
